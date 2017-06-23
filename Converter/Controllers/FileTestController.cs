ff76ae3de728436a4152de6ede363ba5f24ea613
Agrendalath <piotr@surowiec.it>
2017-06-23 03:54:50 +0200

    Handling and viewing uploaded files.

--- Converter/Controllers/FileTestController.cs
+++ Converter/Controllers/FileTestController.cs
@@ -6,27 +6,49 @@ using System.Web;
 using System.Web.Mvc;
 using Spire.Doc;
 using Spire.Doc.Documents;
+using Microsoft.AspNet.Identity;
 //using Spire.Pdf;
 
 namespace Converter.Controllers
 {
+    [Authorize]
     public class FileTestController : Controller
     {
         // GET: FileTest
         public ActionResult Index()
         {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "uploads/" + User.Identity.GetUserId().ToString();
+            Directory.CreateDirectory(path);
+
             foreach (string upload in Request.Files)
             {
-                string path = AppDomain.CurrentDomain.BaseDirectory + "uploads/";
+                if (Request.Files[upload].ContentLength == 0)
+                    continue;
+
                 string filename = Path.GetFileName(Request.Files[upload].FileName);
                 string filePath = Path.Combine(path, filename);
                 Request.Files[upload].SaveAs(filePath);
                 Document document = new Document();
                 document.LoadFromFile(filePath);
 
-                document.SaveToFile(Path.ChangeExtension(filePath, "pdf"), FileFormat.PDF);
+                document.SaveToFile(Path.ChangeExtension(filePath, "pdf"), FileFormat.PDF);                
             }
+
+            ViewBag.Files = Directory.GetFiles(path).Select(Path.GetFileName).ToArray();
             return View();
+        }
+
+        public FileStreamResult Get(string fileName)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "uploads/" + User.Identity.GetUserId().ToString() + "/";
+            try
+            {
+                return File(new FileStream(path + fileName, FileMode.Open), "text/plain", fileName);
+            }
+            catch (IOException e)
+            {
+                return null;
+            }
         }
     }
 }
